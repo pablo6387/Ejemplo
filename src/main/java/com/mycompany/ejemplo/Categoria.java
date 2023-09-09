1ce9f36d5f4addfcda29989bb082fc87c95bfa0b
pablo6387 <56855366+pablo6387@users.noreply.github.com>
2023-09-09 16:48:34 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/ejemplo/Categoria.java
@@ -0,0 +1,15 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.ejemplo;
+
+/**
+ *
+ * @author plopez
+ */
+public enum Categoria {
+    COMESTIBLE,
+    LIMPIEZA,
+    PEFUMERIA
+}
---
+++ src/main/java/com/mycompany/ejemplo/Producto.java
@@ -0,0 +1,79 @@
+
+package com.mycompany.ejemplo;
+
+/**
+ *
+ * @author plopez
+ */
+public class Producto implements Comparable<Producto>{
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private Categoria rubro;
+
+    public Producto(int codigo, String descripcion, double precio, int stock, Categoria rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+
+    public Producto() {
+    }
+    
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Categoria getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(Categoria rubro) {
+        this.rubro = rubro;
+    }
+
+    @Override
+    public int compareTo(Producto t) {
+        if(codigo==t.codigo){
+            return 0;
+        }else if (codigo>t.codigo){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+    
+    
+}
